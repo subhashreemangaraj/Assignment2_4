badfd9d480c00cb176f464d599318212e0b2575d
subhashreemangaraj <subhashree.202@gmail.com>
2016-11-20 23:29:35 +0530

    Add files via upload

---
+++ Assignment2_4_highest.java
@@ -0,0 +1,29 @@
+
+public class Assignment2_4_highest {
+	
+public static void main(String[] args) {
+	
+	int a = 10;
+	int b = 111;
+	int c = 57;
+	
+	if (a > b && a > c)
+	{
+		
+		System.out.println("highest num:" +a);
+	}
+	
+		else if(b > a && b> c)
+		{
+			
+			System.out.println("highest num:" +b);
+			
+		}
+		
+		else {
+			System.out.println("highest num" +c);
+		}
+		
+	
+}
+}
